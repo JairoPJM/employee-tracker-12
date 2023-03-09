1863c05c12e7e01c5e8cb709e1a26b37a22a63f9
JairoPJM <Jairomacassi@gmail.com>
2023-03-09 16:25:45 -0500

    finished with server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,23 +3,23 @@ VALUES("Engineering"),("Legal"),("Finance"),("Sales");
 
 INSERT INTO role(title, salary, department_id)
 VALUES
- ("Sales Lead", 100000, 4),
-       ("Salesperson", 80000, 4),
-       ("Lead Engineer", 150000, 1 ),
-       ("Software Engineer", 120000, 1),
-       ("Account Manager", 160000, 3 ),
-       ("Accountant", 125000, 3),
-       ("Legal Team Lead", 250000, 2),
+ ("Sales Lead", 200000, 4),
+       ("Salesperson", 31300, 4),
+       ("Lead Engineer", 250000, 1 ),
+       ("Software Engineer", 180000, 1),
+       ("Account Manager", 80000, 3 ),
+       ("Accountant", 100000, 3),
+       ("Legal Team Lead", 180000, 2),
        ("Lawyer", 190000, 2);
 
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
 ('John', 'Doe', 1, null),
-       ('Mike', 'Chan', 2, 1),
-       ('Ashley', 'Rodriguez', 3, null),
-       ('Laura', 'Trapaga', 4, 3),
-       ('Kunal', 'Singh', 5, null),
-       ('Malia', 'Brown', 6, 5),
-       ('Sarah', 'Lourd', 7, null),
-       ('Calvin', 'Chern', 8, 7);
+       ('Jairo', 'Macassi', 2, 1),
+       ('Brian', 'Rodriguez', 3, null),
+       ('Fredrick', 'Post', 4, 3),
+       ('Justin', 'Green', 5, null),
+       ('Tyler', 'Brown', 6, 5),
+       ('Floyd', 'Lourd', 7, null),
+       ('Peter', 'Lee', 8, 7);

fc0ec56b1aae685db49406356e8b21a296eb2da7
JairoPJM <Jairomacassi@gmail.com>
2023-03-09 12:50:36 -0500

    added .env and seeded db

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,12 @@ CREATE DATABASE department_db;
 
 USE department_db;
 
--- table for department
+
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
--- table for role
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
@@ -17,7 +16,7 @@ CREATE TABLE role (
     department_id INT
 );
 
--- table for employee
+
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
